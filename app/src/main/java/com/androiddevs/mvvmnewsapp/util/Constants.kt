4e083c7d0968ffb8b929b9e3f9f47352dde9c5c3
Rodrigo Bellanti <rdrg.000@gmail.com>
2023-02-21 02:23:50 -0300

    showing articles in webview

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         // not important, app is just for practice
         const val API_KEY = "643003e4c02242bfa26a783d98da0a65"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
